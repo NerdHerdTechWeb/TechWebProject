31a58433d6a9c02fa8e3e7f84a4d6a5063606ca7
r1soft-stack <riccardo.masetti.work@gmail.com>
2015-06-17 13:54:29 +0200

    meta widget modal

--- scripts/controllers/WidgetMeta.js
+++ scripts/controllers/WidgetMeta.js
@@ -6,7 +6,7 @@
         .module('semanticNotations')
         .controller('WidgetMeta', widgetMeta);
 
-    function widgetMeta(meta, $scope, $window) {
+    function widgetMeta(meta, $scope, $modal, $log, $window) {
 
         // vm is our capture variable
         var vm = this;
@@ -19,5 +19,27 @@
         }, function(error) { // Check for errors
             console.log(error);
         });
+
+        $scope.items = ['item1', 'item2', 'item3'];
+        $scope.open = function (size) {
+
+            var modalInstance = $modal.open({
+                animation: true,
+                templateUrl: '/partials/modals/widgetMetaModal.html',
+                controller: 'WidgetMetaModal',
+                size: size,
+                resolve: {
+                    items: function () {
+                        return $scope.items;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (selectedItem) {
+                $scope.selected = selectedItem;
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
     }
 })();
