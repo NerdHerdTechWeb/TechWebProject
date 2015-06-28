3ae42de58e009f187f744317eb455c7d2a61b9a0
@riccardo <riccardo.masetti.work@gmail.com>
2015-06-28 21:51:40 +0200

    fix minor scope function

--- app/scripts/controllers/WidgetDocumentsList.js
+++ app/scripts/controllers/WidgetDocumentsList.js
@@ -11,22 +11,27 @@
 
         // vm is our capture variable
         var vm = this;
+        $scope.documentsLoaded = [];
 
         vm.documentEntries = [];
 
-        documents.getDocuments().then(function (results) {
-            vm.documentEntries = results;
-        }, function (error) { // Check for errors
-            console.log(error);
-        });
-
-        $scope.getMainDocument = function (link, from) {
-            documents.getDocument(link, from).then(function (results) {
-                vm.documentEntry = results;
+        documents.getDocuments().then(
+            function (results) {
+                vm.documentEntries = results;
             }, function (error) { // Check for errors
                 console.log(error);
-            });
+            }
+        );
 
+        $scope.getMainDocument = function (link, from, data) {
+            documents.getDocument(link, from).then(
+                function (results) {
+                    vm.documentEntry = results;
+                    mainDocAreaFiller(data, results);
+                }, function (error) { // Check for errors
+                    console.log(error);
+                }
+            );
         }
     }
 
@@ -34,14 +39,16 @@
         return {
             restrict: 'AC',
             link: function (scope, element, attrs) {
-                var id = '#mainDocArea';
                 var data = scope.$eval(attrs.insertTab);
                 jQuery(element).on('click', function (event) {
-                    scope.getMainDocument(data.documents.link, data.documents.from);
-                    jQuery(id).append(' <li role="presentation" class="active"><a href="">' + data.documents.title + '</a></li>');
-                    scope.$apply();
+                    scope.getMainDocument(data.documents.link, data.documents.from, data);
                 });
             }
-        };
+        }
+    }
+
+    function mainDocAreaFiller(data, results) {
+        var id = '#mainDocArea';
+        jQuery(id).append('<li role="presentation" class="active"><a href="">' + data.documents.title + '</a></li>');
     }
 })(jQuery);
