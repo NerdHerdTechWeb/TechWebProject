afdf7011bd9030df1ed22257db154f5991ad0a61
Mattia1 <mattiavalli88@gmail.com>
2015-10-07 15:45:40 +0200

    Update Sparql_Client.php

--- app/models/Sparql_Client.php
+++ app/models/Sparql_Client.php
@@ -183,7 +183,7 @@ SELECT ?source
 WHERE{
 	GRAPH <http://vitali.web.cs.unibo.it/raschietto/graph/ltw1525>
 	{
-	 {
+	
 	 ?annotation  rdf:type oa:Annotation.
 		OPTIONAL { ?annotation rsch:type ?watf }       
 		OPTIONAL { ?annotation oa:hasBody ?body }
@@ -195,32 +195,13 @@ WHERE{
   		?annotation oa:hasTarget ?node.
 	    	?node rdf:type oa:SpecificResource ;
             	      oa:hasSource ?source .
-  	{					  
-  		FILTER regex (?watf , 'hasAuthor')
-		FILTER regex (?o_label , '$author')}
-				
-		UNION {
-		FILTER regex (?watf , 'hasURL')
-		FILTER regex (str(?o) , LCASE('$url'))
-			}
-		UNION {
-		FILTER regex (?watf , 'hasTitle')
-  		FILTER regex ((str(LCASE(?o))) , LCASE('$title'))
-			}
-
-		UNION {
-		FILTER regex(?watf , 'hasPublicationYear')
-		FILTER (?o > '$date'^^xsd:date)  		
-			}
-		
-		UNION{
-		FILTER regex (?watf , 'references')
-		FILTER regex (LCASE(?o_label) , LCASE('$cities'))
-		
-	    
-			}
-		}
-}
+  	
+  	{ FILTER ((?watf = 'hasPublicationYear' && ?o > xsd:date('2015-08-15'))  || (?watf = 'hasAuthor' && ?o_label = 'Charles Oppenheim') || (?watf ='hasURL' && regex (str(?o) , LCASE('google'))) || (?watf ='hasTitle' && regex (LCASE(str(?o)) ,LCASE('Repubblica.it Esteri'))) || (?watf ='references' && regex (str(?o) , LCASE('wolska')))                      ) }
+	  	}
+	}
+  }
+  	
+  	
 LIMIT 400";
 
         $this->results = $this->sClient->query($query);
