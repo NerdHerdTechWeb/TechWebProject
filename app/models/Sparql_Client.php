879f5303de39719de4a3cd74e3db54d7723645b9
Mattia1 <mattiavalli88@gmail.com>
2015-10-07 15:49:58 +0200

    Update Sparql_Client.php

--- app/models/Sparql_Client.php
+++ app/models/Sparql_Client.php
@@ -196,10 +196,13 @@ WHERE{
 	    	?node rdf:type oa:SpecificResource ;
             	      oa:hasSource ?source .
   	
-  	{ FILTER ((?watf = 'hasPublicationYear' && ?o > xsd:date('2015-08-15'))  || (?watf = 'hasAuthor' && ?o_label = 'Charles Oppenheim') || (?watf ='hasURL' && regex (str(?o) , LCASE('google'))) || (?watf ='hasTitle' && regex (LCASE(str(?o)) ,LCASE('Repubblica.it Esteri'))) || (?watf ='references' && regex (str(?o) , LCASE('wolska')))                      ) }
-	  	
+  { FILTER ((?watf = 'hasPublicationYear' && ?o > xsd:date('2015-08-15'))  ||
+	 (?watf = 'hasAuthor' && ?o_label = 'Charles Oppenheim') ||
+	  (?watf ='hasURL' && regex (str(?o) , LCASE('google'))) ||
+	   (?watf ='hasTitle' && regex (LCASE(str(?o)) ,LCASE('Repubblica.it Esteri'))) ||
+	    (?watf ='references' && regex (str(?o) , LCASE('wolska')))) }
+	  	}
 	}
-  }
   	
   	
 LIMIT 400";
