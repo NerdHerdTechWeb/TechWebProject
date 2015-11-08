b3f079c8361a2f2463f287034eef23ed38e64843
@riccardo <riccardo.masetti.work@gmail.com>
2015-11-08 18:41:43 +0100

    graph 1542

--- app/scripts/controllers/DocumentsManager.js
+++ app/scripts/controllers/DocumentsManager.js
@@ -106,7 +106,7 @@
         $scope.loadAnnotations = function (source) {
             return fragment.loadAnnotations({
                 source: source,
-                graph: 'http://vitali.web.cs.unibo.it/raschietto/graph/ltw1520'
+                graph: 'http://vitali.web.cs.unibo.it/raschietto/graph/ltw1542'
             }).then(function(results){
                 jQuery('tr').unwrap('tbody');
                 jQuery('#navTabsContainer img:not(.img-replaced)').each(function (i, el) {
